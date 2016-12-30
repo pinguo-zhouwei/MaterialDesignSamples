fc106924d4b73069ba8cb613c9a2312fb433a16f
zhouwei <zhouwei@wepiao.com>
2016-12-30 18:57:24 +0800

    TextInputLayout

--- app/src/main/java/com/zhouwei/md/materialdesignsamples/edit/TextInputSimpleActivity.java
+++ app/src/main/java/com/zhouwei/md/materialdesignsamples/edit/TextInputSimpleActivity.java
@@ -5,7 +5,12 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 
 import com.zhouwei.md.materialdesignsamples.R;
 
@@ -15,7 +20,8 @@ import com.zhouwei.md.materialdesignsamples.R;
 
 public class TextInputSimpleActivity extends AppCompatActivity{
     private TextInputLayout mTextInputLayoutUser,mTextInputLayoutPassword;
-    private TextInputEditText mInputEditTextUser,mInputEditTextPassword;
+    private TextInputEditText mInputEditTextPassword;
+    private EditText mInputEditTextUser;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,19 +31,49 @@ public class TextInputSimpleActivity extends AppCompatActivity{
     }
 
     private void initView() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
         mTextInputLayoutUser = (TextInputLayout) findViewById(R.id.text_input_layout_user);
-        mTextInputLayoutPassword = (TextInputLayout) findViewById(R.id.text_input_layout_password);
-        mInputEditTextUser = (TextInputEditText) findViewById(R.id.text_input_user);
-        mInputEditTextPassword = (TextInputEditText) findViewById(R.id.text_input_password);
         //设置可以计数
         mTextInputLayoutUser.setCounterEnabled(true);
         //计数的最大值
         mTextInputLayoutUser.setCounterMaxLength(20);
 
+        mInputEditTextUser = (EditText) findViewById(R.id.text_input_user);
+
+        mTextInputLayoutPassword = (TextInputLayout) findViewById(R.id.text_input_layout_password);
+        mInputEditTextPassword = (TextInputEditText) findViewById(R.id.text_input_password);
+
+        mInputEditTextPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mTextInputLayoutPassword.setErrorEnabled(false);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mTextInputLayoutPassword.setError("密码错误，请重试");
+                String password = mInputEditTextPassword.getText().toString();
+                if(TextUtils.isEmpty(password)||password.length()<6){
+                    mTextInputLayoutPassword.setError("密码错误不能少于6个字符");
+                }
 
             }
         });
